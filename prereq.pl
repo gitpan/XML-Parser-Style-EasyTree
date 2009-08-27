89d8bfc4e827068a500b43d600c7619c14aea68b
Mons Anderson <mons@cpan.org>
2009-08-27 11:38:22 +0000

    Import of MONS/XML-Parser-Style-EasyTree-0.09 from CPAN.
    gitpan-cpan-distribution: XML-Parser-Style-EasyTree
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         MONS/XML-Parser-Style-EasyTree-0.09.tar.gz
    gitpan-cpan-author:       MONS
    gitpan-cpan-maturity:     released

--- prereq.pl
+++
@@ -1,7 +0,0 @@
-use CPANPLUS::Backend;
-use Module::CPANTS::Generator::Prereq;
-
-my $p = Module::CPANTS::Generator::Prereq->new;
-$p->cpanplus(CPANPLUS::Backend->new);
-$p->directory(".");
-$p->generate;
